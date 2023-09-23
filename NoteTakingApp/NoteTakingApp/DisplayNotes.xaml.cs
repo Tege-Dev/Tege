3e1f2602851c571782efd25d2b381052a3084c1b
Artūras <115102118+CloudNeinn@users.noreply.github.com>
2023-09-23 15:54:33 +0300

    Ikeliu projekta

---
+++ NoteTakingApp/NoteTakingApp/DisplayNotes.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace NoteTakingApp
+{
+    public partial class DisplayNotes : Window
+    {
+        private List<string> Notes;
+
+        public DisplayNotes(List<string> notes)
+        {
+            InitializeComponent();
+
+            Notes = notes;
+            foreach (string note in Notes)
+            {
+                notesListBox.Items.Add(note);
+            }
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string keyword = searchTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(keyword))
+            {
+                List<string> matchingNotes = Notes.Where(note => note.Contains(keyword)).ToList();
+                notesListBox.Items.Clear();
+                foreach (string note in matchingNotes)
+                {
+                    notesListBox.Items.Add(note);
+                }
+            }
+            else
+            {
+                // If the search box is empty, show all notes
+                notesListBox.Items.Clear();
+                foreach (string note in Notes)
+                {
+                    notesListBox.Items.Add(note);
+                }
+            }
+        }
+        private void RevertButton_Click(object sender, RoutedEventArgs e)
+        {
+            notesListBox.Items.Clear();
+            foreach (string note in Notes)
+            {
+                notesListBox.Items.Add(note);
+            }
+            searchTextBox.Text = "";
+        }
+    }
+}
+
---
+++ NoteTakingApp/NoteTakingApp/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NoteTakingApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> Notes;
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (File.Exists("SavedNotes.txt")) Notes = File.ReadAllLines("SavedNotes.txt").ToList();
+            else Notes = new List<string>();
+            NoteVisibilityToggle(Notes);
+        }
+        private void DisplayNotes(object sender, RoutedEventArgs e)
+        {
+            var newDisplayNotes = new DisplayNotes(Notes);
+            newDisplayNotes.Show();
+        }
+        private void ClearNotes(object sender, RoutedEventArgs e)
+        {
+            Notes.Clear();
+            NoteVisibilityToggle(Notes);
+        }
+
+        private void AddNote(object sender, RoutedEventArgs e)
+        {
+            var newAddNote = new AddNote(Notes, this);
+            newAddNote.Show();
+        }
+        public void NoteVisibilityToggle(List<string> Notes)
+        {
+            if (Notes.Count > 0) pageDisplay.Visibility = Visibility.Visible;
+            else pageDisplay.Visibility = Visibility.Hidden;
+        }
+    }
+}
