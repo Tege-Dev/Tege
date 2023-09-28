9d98ebfd26499d090f5283bd71a637e20af315f5
Artūras <115102118+CloudNeinn@users.noreply.github.com>
2023-09-28 19:59:08 +0300

    Pridedu normalu \

---
+++ NoteTakingApp/NoteTakingApp/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteTakingApp
+{
+    public class Note
+    {
+        public int Number { get; set; }
+        public string Author { get; set; }
+        public string Theme { get; set; }
+        public string Content { get; set; }
+
+        public Note(int number, string author, string theme, string content)
+        {
+            Number = number;
+            Author = author;
+            Theme = theme;
+            Content = content;
+        }
+    }
+}
