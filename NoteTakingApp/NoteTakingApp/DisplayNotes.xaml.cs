50f624bf66341fe0535a2a2087c0813337a14e08
Artūras <115102118+CloudNeinn@users.noreply.github.com>
2023-10-11 19:03:40 +0300

    Added functionality for note sorting by theme/author/number

--- NoteTakingApp/NoteTakingApp/DisplayNotes.xaml.cs
+++ NoteTakingApp/NoteTakingApp/DisplayNotes.xaml.cs
@@ -1,20 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace NoteTakingApp
 {
     public partial class DisplayNotes : Window
     {
         private List<Note> Notes;
+        private string sortedColumn;
+        private bool ascendingOrder;
 
         public DisplayNotes(List<Note> notes)
         {
             InitializeComponent();
 
             Notes = notes;
-            foreach (Note note in Notes)
+            sortedColumn = "";
+            ascendingOrder = true;
+            DisplayNotesInListBox(Notes);
+        }
+
+        private void DisplayNotesInListBox(List<Note> notes)
+        {
+            notesListBox.Items.Clear();
+            foreach (Note note in notes)
             {
                 notesListBox.Items.Add(note.Number.ToString() + ": " + note.Author + " - " + note.Theme + ". " + note.Content);
             }
@@ -26,32 +37,49 @@ namespace NoteTakingApp
             if (!string.IsNullOrWhiteSpace(keyword))
             {
                 List<Note> matchingNotes = Notes.Where(note => note.Theme.Contains(keyword)).ToList();
-
-                notesListBox.Items.Clear();
-                foreach (Note note in matchingNotes)
-                {
-                    notesListBox.Items.Add(note.Number.ToString() + ": " + note.Author + " - " + note.Theme + ". " + note.Content);
-                }
+                DisplayNotesInListBox(matchingNotes);
             }
             else
             {
-                // If the search box is empty, show all notes
-                notesListBox.Items.Clear();
-                foreach (Note note in Notes)
-                {
-                    notesListBox.Items.Add(note.Number.ToString() + ": " + note.Author + " - " + note.Theme + ". " + note.Content);
-                }
+                DisplayNotesInListBox(Notes);
             }
         }
 
         private void RevertButton_Click(object sender, RoutedEventArgs e)
         {
-            notesListBox.Items.Clear();
-            foreach (Note note in Notes)
-            {
-                notesListBox.Items.Add(note.Number.ToString() + ": " + note.Author + " - " + note.Theme + ". " + note.Content);
-            }
+            DisplayNotesInListBox(Notes);
             searchTextBox.Text = "";
+        }
+
+        private void SortButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string columnName = button.Tag.ToString();
+
+            if (sortedColumn == columnName)
+            {
+                ascendingOrder = !ascendingOrder;
+            }
+            else
+            {
+                sortedColumn = columnName;
+                ascendingOrder = true;
+            }
+
+            switch (columnName)
+            {
+                case "Number":
+                    Notes.Sort((note1, note2) => ascendingOrder ? note1.Number.CompareTo(note2.Number) : note2.Number.CompareTo(note1.Number));
+                    break;
+                case "Author":
+                    Notes.Sort((note1, note2) => ascendingOrder ? note1.Author.CompareTo(note2.Author) : note2.Author.CompareTo(note1.Author));
+                    break;
+                case "Theme":
+                    Notes.Sort((note1, note2) => ascendingOrder ? note1.Theme.CompareTo(note2.Theme) : note2.Theme.CompareTo(note1.Theme));
+                    break;
+            }
+
+            DisplayNotesInListBox(Notes);
         }
     }
 }
