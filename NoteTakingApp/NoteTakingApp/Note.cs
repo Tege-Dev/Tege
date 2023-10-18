daeb84473213f403200167d6eabfd2712bf30541
Artūras <115102118+CloudNeinn@users.noreply.github.com>
2023-10-18 23:29:26 +0300

    box/unbox + tag variable

--- NoteTakingApp/NoteTakingApp/Note.cs
+++ NoteTakingApp/NoteTakingApp/Note.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace NoteTakingApp
 {
     public class Note : IComparable<Note>
@@ -13,16 +12,27 @@ namespace NoteTakingApp
         public string Author { get; set; }
         public string Theme { get; set; }
         public string Content { get; set; }
+        public string Tag { get; set; } 
 
-        public Note(int number, string author, string theme, string content)
+        public Note(int number, string author, string theme, string content, string tag = null)
         {
             Number = number;
             Author = author;
             Theme = theme;
             Content = content;
+            Tag = tag;
+        }
+        public object NumberToObject()
+        {
+            object boxedNumber = Number;
+            return boxedNumber;
         }
 
-        // Implement IComparable<Note>
+        public void NumberFromObject(object boxedNumber)
+        {
+            Number = (int)boxedNumber;
+        }
+
         public int CompareTo(Note other)
         {
             return this.Number.CompareTo(other.Number);
